64685eb6b312081f5cca5950f8c53328057ee54f
zeroed <edd.rossi@gmail.com>
2013-08-10 16:36:47 +0200

    soa_section with revision (wip)

--- app/models/soa_section.rb
+++ app/models/soa_section.rb
@@ -2,6 +2,7 @@ class SoaSection < ActiveRecord::Base
   belongs_to :user
   belongs_to :dns_zone
 
+  validates :dns_zone, presence: true
   validates :user_id, presence: true
   validates :primary_domain_name, :presence => true
   validates :serial_number, :refresh, :retry, :expire, :negative_caching, :numericality => { :only_integer => true }
@@ -13,6 +14,7 @@ class SoaSection < ActiveRecord::Base
     self.refresh ||= 0
     self.retry ||= 0
     self.expire ||= 0
+    self.revision ||= 1
     self.negative_caching ||= 0
   end
 
@@ -21,7 +23,7 @@ class SoaSection < ActiveRecord::Base
   end
 
   def max_revision
-    (SoaSection.maximum(:revision) || 0) + 1
+    (SoaSection.where('user_id' => user_id).maximum(:revision) || 0) + 1
   end
 
 end
