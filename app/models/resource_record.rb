86bcc5e7fc532f2b26d7cf83143d47487d1f514c
zeroed <edd.rossi@gmail.com>
2013-08-18 18:23:43 +0200

    capitalize definitions

--- app/models/resource_record.rb
+++ app/models/resource_record.rb
@@ -23,11 +23,11 @@ class ResourceRecord < ActiveRecord::Base
 
     def definitions
       {
-        'A' => ['address record','Returns a 32-bit IPv4 address, most commonly used to map hostnames to an IP address of the host, but also used for DNSBLs, storing subnet masks in RFC 1101, etc.'],
-        'CNAME' => ['Canonical name record','Alias of one name to another: the DNS lookup will continue by retrying the lookup with the new name.'],
-        'NS' => ['name server record','Delegates a DNS zone to use the given authoritative name servers'],
-        'MX' => ['mail exchange record','Maps a domain name to a list of message transfer agents for that domain'],
-        'PTR' => ['pointer record','Pointer to a canonical name. Unlike a CNAME, DNS processing does NOT proceed, just the name is returned. The most common use is for implementing reverse DNS lookups, but other uses include such things as DNS-SD.']
+        'A' => ['Address Record','Returns a 32-bit IPv4 address, most commonly used to map hostnames to an IP address of the host, but also used for DNSBLs, storing subnet masks in RFC 1101, etc.'],
+        'CNAME' => ['Canonical Name Record','Alias of one name to another: the DNS lookup will continue by retrying the lookup with the new name.'],
+        'NS' => ['Name Server Record','Delegates a DNS zone to use the given authoritative name servers'],
+        'MX' => ['Mail Exchange Record','Maps a domain name to a list of message transfer agents for that domain'],
+        'PTR' => ['Pointer Record','Pointer to a canonical name. Unlike a CNAME, DNS processing does NOT proceed, just the name is returned. The most common use is for implementing reverse DNS lookups, but other uses include such things as DNS-SD.']
       }
     end
 
