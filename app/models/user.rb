a3af463e5c3b62b98fb968bf931ea62c83242d5f
zeroed <edd.rossi@gmail.com>
2013-08-22 23:01:18 +0200

    rails admin configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,18 +8,19 @@ class User < ActiveRecord::Base
   has_many :resource_records
 
   has_and_belongs_to_many :roles
-  
+
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :trackable, :validatable
 
   before_create :set_default_role
+
+  def admin?
+    roles.map(&:name).include? 'admin'
+  end
 
   private
   def set_default_role
     self.roles << Role.find_by_name('user')
   end
 
-  def admin?
-  	roles.map(&:name).include? 'admin'
-  end
 end
