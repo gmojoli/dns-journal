f71ba43197021e3bc6c052967e7246d36612e8c7
zeroed <edd.rossi@gmail.com>
2013-09-08 16:27:49 +0200

    dns_zone controller specs

--- spec/controllers/dns_zone_controller_spec.rb
+++ spec/controllers/dns_zone_controller_spec.rb
@@ -13,30 +13,40 @@ describe DnsZonesController do
       controller.stub(:current_user).and_return @dns_zone.user
     end
 
+    def do_update(attrs)
+      put :update, {:domain_id => @dns_zone.domain.slug, :id => @dns_zone.id, :dns_zone => attrs}
+    end
+
     context "with an existing domain and a dns_zone" do
 
       it "create a new dns_zone when updating" do
         lambda do
-          put :update, {:domain_id => @dns_zone.domain.slug, :id => @dns_zone.id, :dns_zone => @attr}
+          do_update @attr
         end.should change(DnsZone, :count).by(1)
-        @dns_zone[:origin].should_not eq @attr[:origin]
         @dns_zone[:origin].should eq 'foo.com'
         DnsZone.last[:origin].should eq @attr[:origin]
       end
 
       it "create a dup for every resouce_record" do
-        # expect{get :index}.to raise_error(CanCan::AccessDenied)
         lambda do
-          put :update, {:domain_id => @dns_zone.domain.slug, :id => @dns_zone.id, :dns_zone => @attr}
+          do_update @attr
         end.should change(ResourceRecord, :count).by(@dns_zone.resource_records.count)
       end
 
       it "create a dup for a soa section" do
         lambda do
-          put :update, {:domain_id => @dns_zone.domain.slug, :id => @dns_zone.id, :dns_zone => @attr}
+          do_update @attr
         end.should change(SoaSection, :count).by(1)
       end
+    end
 
+    context 'updating a dns_zone with invalid attributes' do
+      it 'rerender the edit template' do
+        invalid_attrs = { :origin => "bar.com", :ttl => "11", :admin_email => 'invalid[at]email.com' }
+        do_update invalid_attrs
+        response.should render_template('edit')
+        flash[:alert].should match("failed")
+      end
     end
 
   end
