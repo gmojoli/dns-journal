54b22416d87ff049c1acd19c123ac5ceded65a20
zeroed <edd.rossi@gmail.com>
2013-09-29 16:33:37 +0200

    Airbrake initializer

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,6 +1,12 @@
 # https://github.com/airbrake/airbrake
 Airbrake.configure do |config|
-  config.api_key = 'cc6d61734ab784f6fa615f0b64dc123b'
+
+  # Load the API key from a file in the repo
+  api_key = YAML.load_file(File.expand_path '../../airbrake.yml', __FILE__)['api_key']
+  # Load the API key from the environment
+  # api_key = ENV['AIRBRAKE_API_KEY'] or raise "No API key in ENV"
+  puts "Initializing airbrake: api-key: #{api_key}"
+
   # Collect Errors from your Development Environment
   config.development_environments = []
 
