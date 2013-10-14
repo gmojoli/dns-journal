8fa47d12bab2d80c6b20bbef6fc317e49afb3aa7
zeroed <edd.rossi@gmail.com>
2013-10-15 00:22:57 +0200

    Configs.

--- config/application.rb
+++ config/application.rb
@@ -8,17 +8,23 @@ Bundler.require(:default, Rails.env)
 
 module DnsJournal
   class Application < Rails::Application
-	# Settings in config/environments/* take precedence over those specified here.
-	# Application configuration should go into files in config/initializers
-	# -- all .rb files in that directory are automatically loaded.
+  	# Settings in config/environments/* take precedence over those specified here.
+  	# Application configuration should go into files in config/initializers
+  	# -- all .rb files in that directory are automatically loaded.
 
-	# Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-	# Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-	# config.time_zone = 'Central Time (US & Canada)'
+  	# Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+  	# Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+  	# config.time_zone = 'Central Time (US & Canada)'
 
-	# The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-	# config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-	# config.i18n.default_locale = :de
+  	# The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+  	# config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+  	# config.i18n.default_locale = :de
+
+    config.app_generators do |g|
+      # g.orm :datamapper, :migration => true
+      g.template_engine :haml
+      # g.test_framework :rspec
+    end
 
     config.assets.initialize_on_precompile = false
 
