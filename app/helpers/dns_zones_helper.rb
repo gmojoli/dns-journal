aad241c2098dfcc91beffa6e6718ca4974852bd7
zeroed <edd.rossi@gmail.com>
2013-11-03 17:36:18 +0100

    Add helper for readable date.

--- app/helpers/dns_zones_helper.rb
+++ app/helpers/dns_zones_helper.rb
@@ -1,7 +1,22 @@
 module DnsZonesHelper
-
   def print_errors
      @dns_zone.errors.messages
   end
 
+  def seconds_to_readable_time(seconds)
+    mm, ss = seconds.divmod(60)
+    hh, mm = mm.divmod(60)
+    dd, hh = hh.divmod(24)
+    string =
+    if dd > 0
+      "%d days, %d hours, %d minutes, %d seconds" % [dd, hh, mm, ss]
+    elsif hh > 0
+      "%d hours, %d minutes, %d seconds" % [hh, mm, ss]
+    elsif mm > 0
+      "%d minutes, %d seconds" % [mm, ss]
+    else
+      "%d seconds" % ss
+    end
+    string
+  end
 end
